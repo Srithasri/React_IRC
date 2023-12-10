fe6a7d06417f38cc0cbe4422d0a4816e4b58d01b
Srithasri <sritharaja10@gmail.com>
2023-12-10 22:00:09 +0530

    Day2

---
+++ Day2/sampleapp/src/App.js
@@ -0,0 +1,23 @@
+import './Assests/Css/style.css'
+function App()
+{
+    return(
+        <div>
+           
+        <form className = "container" >
+        <h3>Registration Form</h3>
+        <input type="text" name="username" id="username" placeholder="Name" className="auth"/>
+        <input type="email" name="" id="" placeholder="email" className="auth"/>
+        <input type="password" name="" id="pwd1" placeholder="password" className="auth"/>
+        <input type="password" name="" id="pwd2" placeholder="confirm password" className="auth"/>
+        <input type="datetime-local" name="" id="" className="auth"/>
+        <input type="number" name="" id="" placeholder="phone no" className="auth"/>
+        <input type="submit" value="Register" className="btn" />
+        <button  className="i">showuser</button>
+        
+    </form>
+
+        </div>
+    )
+}
+export default App
