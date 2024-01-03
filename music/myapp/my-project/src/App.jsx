563d76a72d4dc1bef1d0e1293f650dd88cebc916
Srithasri <sritharaja10@gmail.com>
2024-01-03 06:33:51 +0530

    update

---
+++ music/myapp/my-project/src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+  <>
+    <div className="h-screen w-screen flex items-center justify-center gap-2 bg-slate-200">
+   
+    <div className="border-2  bg-blue-50 h-60 w-60 flex justify-center items-center shadow-xl bg-cover" style={{backgroundImage:"url('https://i.scdn.co/image/ab67616d0000b273cbb852c7ef0daf9f87816138')"}}></div>
+    <div className="border-2  bg-blue-50 h-60 w-60 flex justify-center items-center shadow-xl bg-cover" style={{backgroundImage:"url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXD9mV1CPKwATMujgKHmsaXyggbyswKXJ3ZdTDcZZ0nbVbbZ1hU_pLcQ4LPdtX1rh1x8M&usqp=CAU')"}}></div>
+    <div className="border-2 bg-blue-50 h-60 w-60 flex justify-center items-center shadow-xl bg cover" style={{backgroundImage:"url('https://wp-socialnation-assets.s3.ap-south-1.amazonaws.com/wp-content/uploads/2023/04/25144317/Arijit-Singh-Wallpaper__-Arijitians-HD-IMAGES__.jpeg')"}}>Hello world!</div>
+    <div className="border-2 bg-blue-50 h-60 w-60 flex justify-center items-center shadow-xl" style={{backgroundImage:"url('')"}}></div>
+   
+    </div>
+    </>
+  )
+}
