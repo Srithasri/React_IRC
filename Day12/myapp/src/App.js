eb41e216409c6890c296245acfaa01c095cb7be7
Srithasri <sritharaja10@gmail.com>
2024-07-23 20:09:37 +0530

    init

--- Day12/myapp/src/App.js
+++ Day12/myapp/src/App.js
@@ -1,13 +1,10 @@
-// import logo from './logo.svg';
-// import './App.css';
+import logo from './logo.svg';
+import './App.css';
 import Counter from './Components/Counter'
 function App() {
   return (
-    // <div className="App">
-    <>
-    <Counter/>
-    
-      {/* <header className="App-header">
+    <div className="App">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -20,9 +17,9 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
-    {/* // </div> */}
-    </>
+      </header>
+      <Counter/>
+    </div>
   );
 }
 
---
+++ Day12/myapp/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+           <p>Sritha T</p>
+           <p>727822TUCS235</p>
+
+    </div>
+  )
+}
+export default About
+  
